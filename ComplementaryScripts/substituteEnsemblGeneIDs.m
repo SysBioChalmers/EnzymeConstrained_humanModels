3510496c828d615582c89250864915508ee039d6
Ivan Domenzain <ivand@chalmers.se>
2018-02-01 00:13:25 +0100

    fix/model-gene.prop: ENSEMBL->UNIPROT codes

--- ComplementaryScripts/substituteEnsemblGeneIDs.m
+++ ComplementaryScripts/substituteEnsemblGeneIDs.m
@@ -9,51 +9,25 @@
 % Ivan Domenzain. Last edited: 2017-10-17
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function model = substituteEnsemblGeneIDs(model)
-    path      = '/Users/ivand/Box Sync/EC-HMR/GECKO_hsa/Databases';
-    
     %Equivalences table between ENSEMBL and HGNC gene IDs for the organism
     %previously downloaded from:   http://www.ensembl.org/biomart,
     %including the fields [Gene stable ID/Gene name/HGNC ID/ Swissprot ID]
     file_name = 'mart_export.txt';
-    cd (path)
+    cd ../Databases/ENSEMBL
     fID       = fopen(file_name);
     data      = textscan(fID,'%s %s %s %s','delimiter',',');
     fclose('all');    
-    model.grRules_ENSEMBL = model.grRules;
-    
-    for i=1:length(model.grRules)
-        if ~isempty(model.grRules{i})
-            %disp(model.grRules{i})
-            str_cells = strsplit(model.grRules{i},' ');
-            grRule    = [];
-            %Decomposes the grRule into individual genes
-            for j=1:length(str_cells)
-                str = (str_cells{j});
-                if ~strcmpi(str,'or') && ~strcmpi(str,'and')
-                    ensemblID = replace(str,'(','');
-                    ensemblID = replace(ensemblID,')','');
-                    index     = indexes_string(data{1},ensemblID);
-                    %If the gene is found in the ENSEMBL iDs it is replaced
-                    %by its corresponding gene name
-                    if ~isempty(index)
-                        str_cells{j} = replace(str,ensemblID,data{2}{index});
-                    end
-                end
-                grRule = strcat(grRule,str_cells{j});
-                if j<length(str_cells)
-                    grRule = strcat(grRule,{' '});
-                end
-            end
-            model.grRules{i} = char(grRule);
+    model.genes_ENSEMBL = model.genes;
+
+    for i=1:length(model.genes)
+        index     = find(strcmpi(data{1},model.genes(i)),1);
+        %If the gene is found in the ENSEMBL iDs it is replaced
+        %by its corresponding gene name
+        if ~isempty(index)
+            model.genes(i) = data{2}(index);
+            
         end
-        disp(strcat('ready with grRule #',string(i)))
+        disp(strcat('ready with gene #',string(i)))
     end
-end
-    
+ end    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Function that receives a string and a cell array and returns the indexes
-% in which the string appears on the array.
-function matching = indexes_string(cell_array,str)
-    matching  = strfind(cell_array,str);
-    matching = find(~cellfun(@isempty,matching),1);
-end
