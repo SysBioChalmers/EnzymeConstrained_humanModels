d7dab3961cb9649a0bd61627006cf981d3c40b5c
Ivan Domenzain <ivand@chalmers.se>
2018-04-04 16:16:48 +0200

    feat: enhanceHepG2model
    function for converting HepG2model to an enzyme constrained model (ecHepG2GEM).

---
+++ ComplementaryScripts/ecHepG2GEM/enhanceHepG2model.m
@@ -0,0 +1,13 @@
+function ecHepG2Model = enhanceHepG2model
+% enhanceHepG2model
+%   Converts the HepG2 specific GEM to an Enzyme constrained version
+%
+%
+%   Ivan Domenzain, 2018-04-04
+%
+    current = pwd;
+    cd ../../models/HepG2
+    %Import HepG2 model xml file and save it as a matlab structure
+    HepG2model = importModel('Hep-G2.xml');
+    save('HepG2model.mat','HepG2model')
+end
