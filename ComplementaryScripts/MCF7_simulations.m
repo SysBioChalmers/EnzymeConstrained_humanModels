ccbb370c5bfd3207ecdfaa6062d51a7f75749b61
Ivan Domenzain <ivand@chalmers.se>
2018-09-27 14:38:42 +0200

    feat: Added master script for MCF7 model simulations

---
+++ ComplementaryScripts/MCF7_simulations.m
@@ -0,0 +1,27 @@
+%Load MCF7 ecModel
+cd ../models/MCF-7/ecMCF7
+load('MCF7_ecModel_batch.mat')
+model = MCF7_ecModel_batch;
+cd ../../../ComplementaryScripts
+
+metList = {'acetyl-CoA','malonyl-CoA','CoA'};
+[resultsProduction_OE,~] = metEng_TargetsFinder(model,'HMR_9034',metList,'production','cytosol','OE');
+metList = [metList,'growth'];
+metList = strrep(metList,'-','_');
+rows    = [{'Yields'};model.genes];
+%Write table file in the results folder
+resultsProduction_OE = num2cell(resultsProduction_OE);
+T = cell2table(resultsProduction_OE,'VariableNames',metList,'RowNames',rows);
+cd ../Results
+writetable(T,'resultsProduction_OE.txt')
+%Repeat again but using the consumption strategy
+cd ../ComplementaryScripts
+metList = {'pyruvate','acetate','citrate','acetyl-CoA','malonyl-CoA'};
+[resultsConsumption_OE,~] = metEng_TargetsFinder(model,'HMR_9034',metList,'consumption','cytosol','OE');
+metList = [metList,'growth'];
+metList = strrep(metList,'-','_');
+rows    = [{'Yields'};model.genes];
+resultsConsumption_OE = num2cell(resultsConsumption_OE);
+T = cell2table(resultsConsumption_OE,'VariableName',metList);
+cd ../Results
+writetable(T,'resultsConsumption_OE.txt')
