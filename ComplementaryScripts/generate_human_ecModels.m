73329d2c6a463bf316a4abd79378ff6aa4b2e6d8
Ivan Domenzain <ivand@chalmers.se>
2019-03-13 00:50:52 +0100

    feat: save original cell line models  in individual folders

--- ComplementaryScripts/generate_human_ecModels.m
+++ ComplementaryScripts/generate_human_ecModels.m
@@ -3,7 +3,13 @@ load('../models/humanGEM_cellLines/11models.mat')
 modelNames = who;
 modelNames = modelNames(~strcmpi(modelNames,'current'));
 for i=1:length(modelNames)
+    cd (current)
     cellName = modelNames{i};
+    mkdir (['../models/' cellName])
+    cd (['../models/' cellName])
+    save([cellName '.mat'])
+    mkdir Data
+    cd (current)
     [ecModel,model_data,kcats] = enhanceGEM_cellLine(cellName);
 end
 cd (current)
