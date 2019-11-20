67c9e1cd24e049941b87023690e322bde65d0a5c
Ivan Domenzain <ivand@chalmers.se>
2019-11-20 15:04:09 +0100

    fix: avoid cd within main enhance_cellLine script

--- ComplementaryScripts/enhanceGEM_cellLine.m
+++ ComplementaryScripts/enhanceGEM_cellLine.m
@@ -1,4 +1,4 @@
-function [ecModel,ecModel_batch] = enhanceGEM_cellLine(cellName)
+function [ecModel,ecModel_batch] = enhanceGEM_cellLine(cellName,GECKO_path)
 %enhanceGEM_cellLine
 % 
 % Function that loads a cell-line or tissue specific human metabolism model
@@ -16,50 +16,40 @@ function [ecModel,ecModel_batch] = enhanceGEM_cellLine(cellName)
 %
 % Usage: [ecModel,ecModel_batch] = enhanceGEM_cellLine(cellName)
 %
-% Ivan Domenzain.      Last edited: 2019-05-15
+% Ivan Domenzain.      Last edited: 2019-11-20
 
 current  = pwd;
 org_name = 'homo sapiens';
 %Load original model 
-model    = load(['../models/' cellName '/' cellName '.mat']);
+model = load(['../models/' cellName '/' cellName '.mat']);
 eval(['model = model.' cellName])
-%model = model.model;
-%%%%%%%%%%%%%%%%%%%%%%%% model preprocessing %%%%%%%%%%%%%%%%%%%%%%%%%%
-%model_modified = ravenCobraWrapper(model);
+%% model preprocessing 
 model_modified = modelModifications(model);
 % Save models
-cd (['../models/' cellName])
-save('model.mat','model')
-save('model_modified.mat','model_modified')
-%%%%%%%%%%%%%%%%%%%%%%%% GECKO modifications %%%%%%%%%%%%%%%%%%%%%%%%%%
+save(['../models/' cellName '/model.mat'],'model')
+save(['../models/' cellName '/model_modified.mat'],'model_modified')
+%% GECKO pipeline 
 % Retrieve kcats & MWs for each rxn in the model from Uniprot database:
-cd ([GECKO_path '/geckomat/get_enzyme_data'])
+cd GECKO/geckomat/get_enzyme_data
 model_data = getEnzymeCodes(model_modified);
 %Tries to Match kinetic coefficients to every reaction with a non empty
 %grRule
 kcats = matchKcats(model_data,org_name);
 %Save ecModel data
-cd (current)
-cd (['../models/' cellName])
-mkdir Data
-cd Data
-save('enzData.mat','model_data','kcats');
-cd (current)
+newDir = ['../../../../models/' cellName '/Data'];
+mkdir(newDir)
+save([newDir '/enzData.mat'],'model_data','kcats');
 %GEt ecModel matlab structure
 model_data = removeFields(model_data);
-cd ([GECKO_path '/geckomat/change_model'])
+cd ../change_model
 ecModel = readKcatData(model_data,kcats);
 % Save output models:
-cd (current)
-cd (['../models/' cellName])
-save('ecModel.mat','ecModel')
-cd ([GECKO_path '/geckomat/limit_proteins'])
+save(['../../../../models/' cellName '/ecModel.mat'],'ecModel')
+cd ../limit_proteins
 % Constrain total protein pool
 Ptotal       = 0.609; %HepG2 total protein content [g prot/gDw]
 protCoverage = 0.5;
 sigma        = 0.5;
 [ecModel_batch,~,~] = constrainEnzymes(ecModel,Ptotal,sigma,protCoverage);
-cd (['../../../../models/' cellName])
-save('ecModel_batch.mat','ecModel_batch')
-cd (current)
+save(['../../../../models/' cellName '/ecModel_batch.mat'],'ecModel_batch')
 end
