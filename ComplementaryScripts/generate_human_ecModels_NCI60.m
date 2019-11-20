14744cd263ad55027bc34e018c75ebdad1213268
Ivan Domenzain <ivand@chalmers.se>
2019-11-20 15:05:04 +0100

    style: main model generator script rearrangemente

--- ComplementaryScripts/generate_human_ecModels_NCI60.m
+++ ComplementaryScripts/generate_human_ecModels_NCI60.m
@@ -4,25 +4,27 @@
 % NCI60 cell-lines) with enzyme-constraints with the use of the GECKO
 % pipeline.
 %
-% Ivan Domenzain.      Last edited: 2019-05-15
+% Ivan Domenzain.      Last edited: 2019-11-20
 
-load('../models/humanGEM_cellLines/11models.mat')
-modelNames = who;
-current    = pwd;
 %Clone GECKO and substitute human-specific scripts
 git('clone https://github.com/SysBioChalmers/GECKO.git')
-GECKO_path =  [current '/GECKO'];
 %Replace scripts in GECKO:
 fileNames = dir('GECKO_humanFiles');
 for i = 1:length(fileNames)
     fileName = fileNames(i).name;
-    if ~strcmp(fileName,'.') && ~strcmp(fileName,'..')
+    if ~strcmp(fileName,'.') && ~strcmp(fileName,'..') && ~strcmp(fileName,'.DS_Store')
         fullName   = ['GECKO_humanFiles/' fileName];
         GECKOpath = dir(['GECKO/**/' fileName]);
         GECKOpath = GECKOpath.folder;
         copyfile(fullName,GECKOpath)
     end
 end
+clear
+clc
+load('../models/humanGEM_cellLines/11models.mat')
+modelNames = who;
+current    = pwd;
+GECKO_path =  [current '/GECKO'];
 %Generate enzyme-constrained models
 for i=1:length(modelNames)
     cd (current)
@@ -33,7 +35,7 @@ for i=1:length(modelNames)
     mkdir Data
     cd (current)
     %Models mat files are saved in their respective folder by enhanceGEM_cellLine
-    [ecModel,ecModel_batch] = enhanceGEM_cellLine(cellName);
+    [ecModel,ecModel_batch] = enhanceGEM_cellLine(cellName,[current '/GECKO']);
 end
 cd (current)
 rmdir('GECKO', 's')
